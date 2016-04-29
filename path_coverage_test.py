3d4be953d999f4696dc329c14e781413b71b0e5a
jessicapelayo <Jep0411@student.cpit.ac.nz>
2016-04-29 14:07:25 +1200

    fix coverage test numbers

--- path_coverage_test.py
+++ path_coverage_test.py
@@ -21,21 +21,21 @@ class PathCoverageTests(unittest.TestCase):
         self.actual = self.model.wash_data()
         self.assertTrue(self.expected == self.actual)
 
-    def test_3_ttttft(self):
+    def test_2_ttttft(self):
         self.model.validator.data_set = ["A601,M,32,231,Nor,132"]
         self.assertTrue(self.model.get_data_set() == self.model.validator.data_set)
         self.expected = []
         self.actual = self.model.wash_data()
         self.assertTrue(self.expected == self.actual)
 
-    def test_4_tttftt(self):
+    def test_3_tttftt(self):
         self.model.validator.data_set = ["A601,M,32,1234,Normal,132"]
         self.assertTrue(self.model.get_data_set() == self.model.validator.data_set)
         self.expected = []
         self.actual = self.model.wash_data()
         self.assertTrue(self.expected == self.actual)
 
-    def test_5_ttfttt(self):
+    def test_4_ttfttt(self):
         self.model.validator.data_set = ["A601,M,111f,123,Normal,123"]
         self.assertTrue(self.model.get_data_set() == self.model.validator.data_set)
         self.expected = []
@@ -43,14 +43,14 @@ class PathCoverageTests(unittest.TestCase):
         self.assertTrue(self.expected == self.actual)
 
 
-    def test_6_tftttt(self):
+    def test_5_tftttt(self):
         self.model.validator.data_set = ["A601,N,32,123,Normal,123"]
         self.assertTrue(self.model.get_data_set() == self.model.validator.data_set)
         self.expected = []
         self.actual = self.model.wash_data()
         self.assertTrue(self.expected == self.actual)
 
-    def test_7_fttttt(self):
+    def test_6_fttttt(self):
         self.model.validator.data_set = ["0000,N,32,123,Normal,123"]
         self.assertTrue(self.model.get_data_set() == self.model.validator.data_set)
         self.expected = []
